7e68bfbd5d50d0278009b16c3d3a672b0ba73d04
Thijs Brobbel <thijsbrobbel@gmail.com>
2021-09-10 23:10:33 +0200

    Add building of VSIX to the test-all.sh script

--- test-all.sh
+++ test-all.sh
@@ -2,3 +2,5 @@
 
 dotnet build Vinyl.slnf
 dotnet test
+
+MSYS_NO_PATHCONV=1 'C:/Program Files (x86)/Microsoft Visual Studio/2019/Professional/MSBuild/Current/Bin/amd64/MSBuild.exe' src/Vinyl.Vsix/Vinyl.Vsix.csproj /v:m
