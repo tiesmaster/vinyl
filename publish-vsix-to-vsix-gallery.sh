bebfc7fc0280f59b992db537b27084126916e930
Thijs Brobbel <thijsbrobbel@gmail.com>
2021-09-11 19:48:12 +0200

    Add script to publish to the informal VSIX Gallery

---
+++ publish-vsix-to-vsix-gallery.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -i \
+    -X POST \
+    'https://www.vsixgallery.com/api/upload?repo=https%3A%2F%2Fgithub.com%2Ftiesmaster%2Fvinyl%2F&issuetracker=https%3A%2F%2Fgithub.com%2Ftiesmaster%2Fvinyl%2Fissues%2F&readmeUrl=https%3A%2F%2Fgithub.com%2Ftiesmaster%2Fvinyl%2Fblob%2Fmain%2FREADME.md' \
+    --form file=@src/Vinyl.Vsix/bin/Release/net472/Vinyl.Vsix.vsix
