51acf05a9f6b2844ce3d2fd532f348ec5162bdf8
Thijs Brobbel <thijsbrobbel@gmail.com>
2021-09-16 22:26:29 +0200

    Extract publish url for VSIX Gallery for better readability

--- publish-vsix-to-vsix-gallery.sh
+++ publish-vsix-to-vsix-gallery.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+publish_url="https://www.vsixgallery.com/api/upload\
+?repo=https%3A%2F%2Fgithub.com%2Ftiesmaster%2Fvinyl%2F\
+&issuetracker=https%3A%2F%2Fgithub.com%2Ftiesmaster%2Fvinyl%2Fissues%2F\
+&readmeUrl=https%3A%2F%2Fraw.githubusercontent.com%2Ftiesmaster%2Fvinyl%2Fmain%2FREADME.md"
+
 curl -i \
     -X POST \
-    'https://www.vsixgallery.com/api/upload?repo=https%3A%2F%2Fgithub.com%2Ftiesmaster%2Fvinyl%2F&issuetracker=https%3A%2F%2Fgithub.com%2Ftiesmaster%2Fvinyl%2Fissues%2F&readmeUrl=https%3A%2F%2Fraw.githubusercontent.com%2Ftiesmaster%2Fvinyl%2Fmain%2FREADME.md' \
+    $publish_url \
     --form file=@src/Vinyl.Vsix/bin/Release/net472/Vinyl.Vsix.vsix
