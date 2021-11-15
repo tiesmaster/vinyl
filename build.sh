99f151b1ecf854d57b6ca54083435752aff914ba
Thijs Brobbel <thijsbrobbel@gmail.com>
2021-11-15 21:47:36 +0100

    WIP

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dotnet build Vinyl.slnf
-dotnet test
+# dotnet build Vinyl.slnf
+# dotnet test
 
 MSYS_NO_PATHCONV=1 "`vswhere -latest -requires Microsoft.Component.MSBuild -find 'MSBuild\**\Bin\MSBuild.exe'`" src/Vinyl.Vsix/Vinyl.Vsix.csproj /v:m
