22587cc36c5f5c78b1e0a6414bba68f9d4cd900c
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-01-27 23:20:21 +0100

    Implement initial version of the refactoring

--- src/Vinyl.CodeFixes/ExpandEfCoreSeededDataRefactoring.cs
+++ src/Vinyl.CodeFixes/ExpandEfCoreSeededDataRefactoring.cs
@@ -1,5 +1,6 @@
 using System.Composition;
 
+using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 
 namespace Vinyl;
@@ -10,6 +11,78 @@ public class ExpandEfCoreSeededDataRefactoring : CodeRefactoringProvider
 {
     public override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
     {
-        throw new NotImplementedException();
+        var root = await context.Document.GetSyntaxRootAsync().ConfigureAwait(false);
+
+        if (root is null)
+        {
+            return;
+        }
+
+        var dbContextClasses = root
+            .DescendantNodes()
+            .OfType<ClassDeclarationSyntax>()
+            .Where(x => x.Identifier.ValueText.EndsWith("DbContext", StringComparison.Ordinal));
+
+        if (!dbContextClasses.Any())
+        {
+            return;
+        }
+
+        var firstDbContext = dbContextClasses.First();
+
+        var modelCreatingMethod = firstDbContext
+            .DescendantNodes()
+            .OfType<MethodDeclarationSyntax>()
+            .SingleOrDefault(x => x.Identifier.ValueText == "OnModelCreating");
+
+        if (modelCreatingMethod is null)
+        {
+            return;
+        }
+
+        var dataSeedExpressions = modelCreatingMethod
+            .DescendantNodes()
+            .OfType<MemberAccessExpressionSyntax>()
+            .Where(x => x.Name.ToString() == "HasData");
+
+        if (!dataSeedExpressions.Any())
+        {
+            return;
+        }
+
+        foreach (var dataSeedExpression in dataSeedExpressions)
+        {
+            var action = CodeAction.Create(
+               "Expand seeded with new value",
+               c => ExpandSeededDataWithNewValueAsync(context.Document, (InvocationExpressionSyntax)dataSeedExpression.Parent, c));
+
+            context.RegisterRefactoring(action);
+        }
+    }
+
+    private static async Task<Document> ExpandSeededDataWithNewValueAsync(
+        Document document,
+        InvocationExpressionSyntax seededDataInvocation,
+        CancellationToken cancellationToken)
+    {
+        var root = await seededDataInvocation.SyntaxTree.GetRootAsync(cancellationToken).ConfigureAwait(false);
+
+        var newValueArgument = seededDataInvocation.ArgumentList.Arguments.Last();
+
+        var arguments = seededDataInvocation.ArgumentList.Arguments;
+
+        var separator = arguments.GetSeparators().First();
+        var argumentsAndSeparatorsList = arguments.GetWithSeparators();
+
+        argumentsAndSeparatorsList = argumentsAndSeparatorsList.Add(separator);
+        argumentsAndSeparatorsList = argumentsAndSeparatorsList.Add(newValueArgument);
+
+        var newArguments = SyntaxFactory.SeparatedList<ArgumentSyntax>(argumentsAndSeparatorsList);
+
+        var newArgumentList = seededDataInvocation.ArgumentList.WithArguments(newArguments);
+
+        var newRoot = root.ReplaceNode(seededDataInvocation.ArgumentList, newArgumentList);
+
+        return document.WithSyntaxRoot(newRoot);
     }
 }
--- tests/Vinyl.UnitTests/ExpandEfCoreSeededDataRefactoringUnitTests.cs
+++ tests/Vinyl.UnitTests/ExpandEfCoreSeededDataRefactoringUnitTests.cs
@@ -6,7 +6,7 @@ namespace Vinyl;
 public class ExpandEfCoreSeededDataRefactoringUnitTests
 {
     [Fact]
-    public async Task NoRefactoring()
+    public async Task TestAsync()
     {
         var source = @"using System;
 
@@ -15,8 +15,8 @@ public class BookDbContext
     public void OnModelCreating(ModelBuilder builder)
     {
         [|builder.Entity<Book>().HasData(
-            new { Id = new Guid(""aa0d97b3-89b8-4a4c-a743-11d9925854dc""), Name = ""Catch-22"", Author = ""Joseph Heller"" });
-        );|]
+            new { Id = new Guid(""aa0d97b3-89b8-4a4c-a743-11d9925854dc""), Name = ""Catch-22"", Author = ""Joseph Heller"" },
+            new { Id = new Guid(""9afaf16c-6fd5-49ac-ae92-0b9b096649ea""), Name = ""Nineteen Eighty-Four"", Author = ""George Orwell"" });|]
     }
 }
 
@@ -45,8 +45,8 @@ public class BookDbContext
     {
         builder.Entity<Book>().HasData(
             new { Id = new Guid(""aa0d97b3-89b8-4a4c-a743-11d9925854dc""), Name = ""Catch-22"", Author = ""Joseph Heller"" },
-            new { Id = new Guid(""aa0d97b3-89b8-4a4c-a743-11d9925854dc""), Name = """", Author = """" });
-        );
+            new { Id = new Guid(""9afaf16c-6fd5-49ac-ae92-0b9b096649ea""), Name = ""Nineteen Eighty-Four"", Author = ""George Orwell"" },
+            new { Id = new Guid(""9afaf16c-6fd5-49ac-ae92-0b9b096649ea""), Name = ""Nineteen Eighty-Four"", Author = ""George Orwell"" });
     }
 }
 
